a11ba5795af50c26cccaff926fb62210bac41625
berkenpas <berkenpasw@gmail.com>
2025-09-18 14:47:31 -0700

    Going with a tree approach I think, with parent-child for nested

---
+++ src/RedRoverPrettyPrint/Core/TreeStringParser.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedRoverPrettyPrint.Core;
+
+public class TreeStringParser
+{
+    public static string Parse(string inputString)
+    {
+        throw new NotImplementedException();
+    }
+}
